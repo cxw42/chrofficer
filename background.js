888633f15e7f2ad931097c37dbfc3bb3d8371ede
Christopher White <cwhite@d3engineering.com>
2019-07-18 17:02:12 -0400

    Revamped for Outlook 365 mid-2019 [WIP]

--- background.js
+++ background.js
@@ -1,24 +1,42 @@
+// Portions Copyright (c) D3 Engineering, LLC.  All Rights Reserved.
+// Show "active" icon when on an Outlook page
 chrome.runtime.onMessage.addListener(function(msg, sender, respond) {
   if (msg !== 'chrofficer-attached') return;
   chrome.browserAction.setIcon({path: '/browser-action-icon-active.png'});
 });
-chrome.webRequest.onCompleted.addListener(function(details) {
+
+// Callback for handling notifications
+function callback(details) {
+  if(details.tabId === -1) { return; }
+  console.log({'Reminder': details});
+
   chrome.windows.getCurrent(function(win) {
     chrome.tabs.get(details.tabId, function(ctab) {
-      if (win.focused && ctab.active) {
-        return;
-      }
+      // TODO make this an option
+      //if (win.focused && ctab.active) {
+      //  return;
+      //}
       chrome.tabs.sendMessage(details.tabId, "calendar_notify", function (resp) {
         resp.reminders.forEach(function(item) {
-          var notification = webkitNotifications.createNotification('/app.png', item.title, item.timeRange);
-          notification.onclick = function() {
-            chrome.tabs.update(details.tabId, {active: true});
-            chrome.windows.update(win.id, {focused: true});
-          };
-          notification.show();
+          let notification_id = chrome.notifications.create(  // TODO add notification ID
+            {
+              type: 'basic',
+              iconUrl: chrome.extension.getURL('/app.png'),
+              title: 'Calendar Reminder',
+              message: item.title,
+              contextMessage: `${item.start_time} (${item.relative_time})`,
+              requireInteraction: true,   // Requires Chrome 50+
+            }
+          );
+          console.log(`Notification ${notification_id} created`);
         });
       });
     });
   });
+} //callback()
 
-}, {urls: ['*://*/owa*calendar_notify*']});
+// Set up to be notified when the notification sound plays
+chrome.webRequest.onBeforeRequest.addListener(
+  callback,
+  {urls: ['*://*/owa*notif*reminder.mp3', '*://*/owa*calendar_notify*']}
+);
--- content.js
+++ content.js
@@ -1,20 +1,56 @@
+// Portions Copyright (c) D3 Engineering, LLC.  All Rights Reserved.
 var map = Array.prototype.map;
 var eleText = function (span) {
   return span.innerText + ' ';
 };
 
+// Tell the background we are up and running
 chrome.runtime.sendMessage("chrofficer-attached");
+
+// Tell the background script about the pending notifications
 chrome.runtime.onMessage.addListener(function(msg, sender, respond) {
   if (msg !== 'calendar_notify') return;
+
   // microsoft hates using IDs, apparently...
-  var eles = document.querySelectorAll('[aria-label="Reminders"] [role="option"]');
-  eles = map.call(eles, function(item) {
-    var attrs = {};
-    attrs.time = map.call(item.querySelectorAll('._f_Fc span'), eleText).join('').trim();
-    attrs.title = item.querySelector('._f_Hc').innerText;
-    var timeRangeEles = item.querySelectorAll('._f_Gc._f_Ic span:first-child')
-    attrs.timeRange = map.call(timeRangeEles, eleText).join('').trim();
-    return attrs;
-  });
+
+  // Find the "Reminders" pane
+  let matches = document.evaluate(
+    '/html/body//div[@aria-live="polite" and .//div/text()="Reminders"]',
+    document,
+    null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+  if(!matches || matches.snapshotLength != 1) {
+    console.warn('Could not find "Reminders" div');
+    return;
+  }
+  let pane = matches.snapshotItem(0);
+
+  // Find the reminders therein
+  matches = document.evaluate(
+    './/button[@data-storybook="reminder"]//div[@class and not(@id)]/text()',
+    pane,
+    null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+  if(!matches || (matches.snapshotLength % 3)) {
+    console.warn('Could not find any reminders');
+    return;
+  }
+
+  // Gather up the text.  Three rows per reminder.
+  const field_names = ['title', 'relative_time', 'start_time'];
+
+  let eles = [];
+  for(let match_idx = 0; match_idx < matches.snapshotLength; ++match_idx) {
+    let ele_idx = Math.floor(match_idx/3);
+    if(!eles[ele_idx]) {
+      eles[ele_idx] = {};
+    }
+
+    eles[ele_idx][field_names[match_idx%3]] =
+      matches.snapshotItem(match_idx).textContent;
+  }
+
+
+  // Send it back to the background script
   respond({msg: "calendar_notify", reminders: eles});
 });
